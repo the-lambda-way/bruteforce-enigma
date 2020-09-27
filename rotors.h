bfe511219c5333a0d761ad7b5998bbbc6dd64a06
the-lambda-way <32426282+the-lambda-way@users.noreply.github.com>
2020-09-27 17:52:44 -0500

    simplified rotor constructor

--- rotors.h
+++ rotors.h
@@ -1,23 +1,12 @@
 #pragma once
 
+#include <algorithm>
 #include <string_view>
 #include <vector>
 
 
 struct Rotor
 {
-     Rotor (std::string pretty_name,
-            std::string_view str,
-            std::string_view turnovers)
-     : pretty_name {pretty_name}
-     {
-          init_rotor(str, forward);
-          init_rotor(inverse_rotor_string(str), reverse);
-
-          turnoverA = turnovers.length() > 0 ? turnovers[0] - 'A' : -1;
-          turnoverB = turnovers.length() > 1 ? turnovers[1] - 'A' : turnoverA;
-     }
-
      // In the enigma machine, using ordinals instead of characters reduces the amount of arithmetic involved.
      // Triplicating the rotor reduces costly modular divisions.
      std::string pretty_name;
@@ -27,26 +16,21 @@ struct Rotor
      int turnoverB;
 
 
-private:
-     // str must have 26 characters
-     void init_rotor (std::string_view str, int* out)
+     Rotor (std::string pretty_name,
+            std::string_view str,
+            std::string_view turnovers)
+     : pretty_name {pretty_name}
      {
-          std::copy(str.begin(), str.end(), out);
-          std::copy(str.begin(), str.end(), out + 26);
-          std::copy(str.begin(), str.end(), out + 52);
+          for (int i = 0; i < 26; ++i)     forward[i] = str[i] - 'A';
+          std::copy(forward, forward + 26, forward + 26);
+          std::copy(forward, forward + 26, forward + 52);
 
-          for (int i = 0; i < 78; ++i)     *out++ -= 'A';
-     }
+          for (int i = 0; i < 26; ++i)     reverse[forward[i]] = i;
+          std::copy(reverse, reverse + 26, reverse + 26);
+          std::copy(reverse, reverse + 26, reverse + 52);
 
-     // Assumes all uppercase and length 26
-     std::string inverse_rotor_string (std::string_view forward)
-     {
-          std::string inverse(26, '-');
-
-          for (int i = 0; i < 26; ++i)
-               inverse[forward[i] - 'A'] = 'A' + i;
-
-          return inverse;
+          turnoverA = turnovers.length() > 0 ? turnovers[0] - 'A' : -1;
+          turnoverB = turnovers.length() > 1 ? turnovers[1] - 'A' : turnoverA;
      }
 };
 
