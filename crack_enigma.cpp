2a5943c525df896417d87bc4ba242f77a255646e
the-lambda-way <32426282+the-lambda-way@users.noreply.github.com>
2020-10-31 03:44:37 -0500

    comments

--- crack_enigma.cpp
+++ crack_enigma.cpp
@@ -12,8 +12,11 @@ Helpful tips:
 Formula for number of encryptions run on smart_decipher:
      num_of_encryptions = num_of_configurations * (26^3 + HighScores_N * 26^2)
 
-Formula for number of encryptions run on bruteforece_decipher:
+Formula for number of encryptions run on bruteforce_decipher:
      num_of_encryptions = num_of_configurations * 26^5
+
+For m4, use 26^4 instead of 26^3, and 26^6 instead of 26^5.
+
 
 Core i7-6500U, 2.50 Ghz
 Speed using Qgram scoring: 207.16 x 10^6 characters/sec
@@ -23,13 +26,13 @@ Speed using IOC scoring: 252.30 x 10^6 characters/sec
 
 int main (int argc, char** argv)
 {
-     // Ciphertext ct = "NPNKANVHWKPXORCDDTRJRXSJFLCIUAIIBUNQIUQFTHLOZOIMENDNGPCB";
-     // Plugboard plug = "";
+     Ciphertext ct   = "NPNKANVHWKPXORCDDTRJRXSJFLCIUAIIBUNQIUQFTHLOZOIMENDNGPCB";
+     Plugboard  plug = "";
 
-     Ciphertext ct = "YXBMXADQBDBAAYIMKDODAYIXNBDQZFJKOLFVEEQBCLUUXDFVQYGKEYBVRHONJKPJMKUNLYLZUKBKJOA"
-                     "JTWVWMOMDPGVXEPUKXBVSGHROFOSBCNKEHEHAKWKOGWTBZFXSYCGSUUPPIZTRTFVCXZVCXTFLMTPTAQ"
-                     "VMREGWSBFZBM";
-     Plugboard plug = "PO ML IU KJ NH YT";
+     // Ciphertext ct = "YXBMXADQBDBAAYIMKDODAYIXNBDQZFJKOLFVEEQBCLUUXDFVQYGKEYBVRHONJKPJMKUNLYLZUKBKJOA"
+     //                 "JTWVWMOMDPGVXEPUKXBVSGHROFOSBCNKEHEHAKWKOGWTBZFXSYCGSUUPPIZTRTFVCXZVCXTFLMTPTAQ"
+     //                 "VMREGWSBFZBM";
+     // Plugboard plug = "PO ML IU KJ NH YT";
 
      Stopwatch sw;
      sw.click();
